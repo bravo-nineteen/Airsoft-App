3e9f2eb89f9bd7adee8c8988321a5b3558d90a5d
bravo-nineteen <my.computer.83@gmail.com>
2026-04-11 07:26:22 +0000

    modified:   airsoft_app_clean/lib/main.dart 	modified:   airsoft_app_clean/pubspec.yaml

--- airsoft_app_clean/lib/main.dart
+++ airsoft_app_clean/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-import 'app/app.dart';
+import 'app.dart';
 import 'core/config/app_config.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await AppConfig.initializeSupabase();
+
+  await Supabase.initialize(
+    url: AppConfig.supabaseUrl,
+    anonKey: AppConfig.supabaseAnonKey,
+  );
+
   runApp(const AirsoftApp());
 }
