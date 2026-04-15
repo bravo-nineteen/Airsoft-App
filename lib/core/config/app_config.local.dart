6237c3975e978a0a931da7a9c0be397253691223
bravo-nineteen <my.computer.83@gmail.com>
2026-04-15 11:09:46 +0000

    Changes to be committed: 	modified:   lib/core/config/app_config.local.dart

--- lib/core/config/app_config.local.dart
+++ lib/core/config/app_config.local.dart
@@ -1,4 +1,4 @@
 class AppConfigLocal {
-  static const String supabaseUrl = 'https://YOUR_PROJECT.supabase.co';
-  static const String supabaseAnonKey = 'YOUR_REAL_ANON_KEY';
+  static const String supabaseUrl = 'https://yznlhjopcqjbsweohptb.supabase.co';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6bmxoam9wY3FqYnN3ZW9ocHRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MDU5NjcsImV4cCI6MjA5MTQ4MTk2N30.Oeom2pEaAvIf2EyFLMeEdIS06CipDFTJ1bsY6BOSG_Q';
 }
