900ee7ed7b594f4a8c3d0a945e027c43c06e2d8c
bravo-nineteen <my.computer.83@gmail.com>
2026-04-17 15:44:08 +0900

    Update auth_gate.dart

--- lib/features/auth/auth_gate.dart
+++ lib/features/auth/auth_gate.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+import '../navigation/app_shell.dart';
 import 'login_screen.dart';
 
 class AuthGate extends StatelessWidget {
-  const AuthGate({
-    super.key,
-    required this.homeBuilder,
-  });
-
-  final Widget Function() homeBuilder;
+  const AuthGate({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +22,7 @@ class AuthGate extends StatelessWidget {
           return const LoginScreen();
         }
 
-        return homeBuilder();
+        return const AppShell();
       },
     );
   }
