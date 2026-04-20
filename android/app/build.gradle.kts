9936424656d8a970bf8bbaa68f17817fbaa43a3c
bravo-nineteen <my.computer.83@gmail.com>
2026-04-20 10:34:13 +0900

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.airsoft_app"
+    namespace = "com.airsoftonlinejapan.fieldops"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "28.2.13676358"
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.airsoft_app"
+        applicationId = "com.airsoftonlinejapan.fieldops"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
